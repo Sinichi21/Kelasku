9674fdaf97b34082b8892729ca1f3a7c83cdf8cb
Sinichi21 <60685687+Sinichi21@users.noreply.github.com>
2023-12-29 20:02:00 +0800

    feat: perbaikan form login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="lex-col flex w-screen h-screen fitems-center justify-between p-20 bg-cover bg-[url('/assets/landing/login1.svg')]">
-      <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex bg-white">
-        <div className="font items-center">
-          <h1 className="text-sky-400 font-sans">Login Sebagai Admin atau User</h1>
+    <div className="w-screen h-screen p-20 bg bg-cover bg-[url('/assets/landing/login1.svg')]">
+      <div className="w-full h-full bg-white justify-center flex flex-col">
+        <div className="mx-48 basis-auto ">
+          <h1 className="text-primary text-3xl font-poppins">Login Sebagai Admin atau User</h1>
+        </div>
+        <div className="flex">
+          <div>
+            <button className="bg-primary text-white p-1 rounded-full ">User</button>
+          </div>
+          <div>
+            <button className="bg-primary text-white p-1 rounded-full ">Admin</button>
+          </div>
         </div>
       </div>
-    </main>
+    </div>
+    // <main className="flex flex-col w-screen h-screen items-center justify-between p-20 bg-cover" style={{ backgroundImage: "url('/assets/landing/login1.svg')" }}>
+    //   <div className="z-10 w-full h-full items-center justify-between font-mono text-sm lg:flex bg-white">
+    //     <div className="font items-center">
+    //       <h1 className="text-sky-400 font-poppins m-auto flex">Login Sebagai Admin atau User</h1>
+    //     </div>
+    //   </div>
+    // </main>
   );
 }
